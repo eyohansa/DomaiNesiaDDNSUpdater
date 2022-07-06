2cf7164cdcd21ba278778a9c80d3e31c33535f69
eyohansa <eric.yohansa@outlook.com>
2022-07-06 11:15:25 +0700

    Log error
    Viewable from Event Viewer

--- DdnsService.cs
+++ DdnsService.cs
@@ -3,16 +3,26 @@ namespace DomaiNesiaDDNSUpdater;
 public class DdnsService
 {
     readonly IConfiguration _config;
+    readonly ILogger<DdnsService> _logger;
     readonly HttpClient _http;
 
-    public DdnsService(IConfiguration config)
-    {
-        _config = config;
-        _http = new HttpClient();
-    }
+    public DdnsService(
+        IConfiguration config,
+        ILogger<DdnsService> logger) =>
+        (_config, _logger, _http) = (config, logger, new HttpClient());
 
     public async Task Update()
     {
-        await _http.GetAsync(_config["DdnsUrl"]);
+        try
+        {
+            await _http.GetAsync(_config["DdnsUrl"]);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, $"{e.Message}");
+
+            // Terminate gracefully and recover accordingly.
+            Environment.Exit(1);
+        }
     }
 }
